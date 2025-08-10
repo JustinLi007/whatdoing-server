c131730cb9f41ad425847b737027801892db983e
justinli007 <justinli2432@gmail.com>
2025-08-09 23:27:28 -0400

    update endpoint paths

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -8,17 +8,11 @@ func (s *Server) RegisterRoutes() *chi.Mux {
 	r := chi.NewRouter()
 	r.Use(s.middleware.Cors)
 
-	r.Group(func(r chi.Router) {
-		r.Use(s.middleware.RequireJwt)
-		r.Get("/contents/anime", s.handlerAnime.GetAllAnime)
-	})
-
 	r.Post("/users/login", s.handlerUsers.Login)
 	r.Post("/users/signup", s.handlerUsers.SignUp)
 	r.Group(func(r chi.Router) {
 		r.Use(s.middleware.RequireJwt)
 		r.Use(s.middleware.RequireUser)
-
 		r.Get("/users/session", s.handlerUsers.CheckSession)
 		r.Delete("/users/session", s.handlerUsers.Logout)
 	})
@@ -26,27 +20,28 @@ func (s *Server) RegisterRoutes() *chi.Mux {
 	r.Group(func(r chi.Router) {
 		r.Use(s.middleware.RequireJwt)
 		r.Use(s.middleware.RequireUser)
-
 		r.Post("/tokens/refresh", s.handlerJwt.RefreshJwt)
 	})
 
-	r.Get("/contents/anime/{contentId}", s.handlerAnime.GetAnime)
+	r.Get("/anime/{contentId}", s.handlerAnime.GetAnime)
+	r.Group(func(r chi.Router) {
+		r.Use(s.middleware.RequireJwt)
+		r.Get("/anime", s.handlerAnime.GetAllAnime)
+	})
 	r.Group(func(r chi.Router) {
 		r.Use(s.middleware.RequireJwt)
 		r.Use(s.middleware.RequireUser)
-
-		r.Post("/contents/anime", s.handlerAnime.NewAnime)
-		r.Put("/contents/anime/{contentId}", s.handlerAnime.UpdateAnime)
+		r.Post("/anime", s.handlerAnime.NewAnime)
+		r.Put("/anime/{contentId}", s.handlerAnime.UpdateAnime)
 	})
 
 	r.Group(func(r chi.Router) {
 		r.Use(s.middleware.RequireJwt)
 		r.Use(s.middleware.RequireUser)
-
-		r.Post("/library/anime", s.handlerProgressAnime.AddToLibrary)
+		r.Post("/progress/anime", s.handlerProgressAnime.AddToLibrary)
 		r.Put("/progress/anime", s.handlerProgressAnime.SetProgress)
-		r.Delete("/library/anime/progress", s.handlerProgressAnime.RemoveProgress)
-		r.Get("/library/anime/progress", s.handlerProgressAnime.GetProgress)
+		r.Delete("/progress/anime", s.handlerProgressAnime.RemoveProgress)
+		r.Get("/progress/anime", s.handlerProgressAnime.GetProgress)
 	})
 
 	return r
